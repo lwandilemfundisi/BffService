2b908495db48402cefb13aa5965959752e6c9160
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-09 07:01:47 +0200

    save

--- BffService.Api/Program.cs
+++ BffService.Api/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddAuthentication(options =>
 })
 .AddOpenIdConnect(options => 
 {
-    options.Authority = "http://localhost:8080/realms/OnlineTicketSalesRealm";
+    options.Authority = "https://localhost:8443/realms/OnlineTicketSalesRealm";
     options.ClientId = "OnlineTicketSalesBff";
     options.ClientSecret = "nluXYrk1ECM08fYYq9HOY1TBPPUaGXME";
     options.ResponseType = OpenIdConnectResponseType.Code;
